0a39dca187015ee2cfa0fcb0c628bc1f06bf3bb3
Léa Buendé <46668755+leabuende@users.noreply.github.com>
2020-05-11 18:38:01 +0200

    adding pictures

--- src/app/inspiration/inspiration.component.ts
+++ src/app/inspiration/inspiration.component.ts
@@ -14,23 +14,28 @@ export class InspirationComponent implements OnInit {
   cards: any = [
     {
       border: this.colors[0],
-      width: 4
+      width: 4,
+   //   src: 'src\assets\59.jpg'
     },
     {
       border:this.colors[1] ,
-      width: 8
+      width: 8,
+     // src: 'src\assets\tate.jpg'
     },
     {
       border:this.colors[2],
-      width: 6
+      width: 6,
+  //    src: 'src\assets\felicita.jpg'
     },
     {
       border:this.colors[3],
-      width: 6
+      width: 6,
+   //   src: 'src\assets\kribi.jpg'
     },
     {
       border:this.colors[0],
-      width: 8
+      width: 8,
+  //    src: 'src\assets\dublin.jpg'
     },
     {
       border:this.colors[1],
